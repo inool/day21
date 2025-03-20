a90d1138a459020c67624e383b759b490c7c753c
inool <inooljariya7@gmail.com>
2025-03-20 14:29:12 +0530

    Add files via upload

---
+++ userdetails.py
@@ -0,0 +1,15 @@
+
+username = input("Enter your username: ")
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+age = int(input("Enter your age: "))
+address = input("Enter your address: ")
+
+
+print("\nUser Details")
+print(f"Username: {username}")
+print(f"Email: {email}")
+print(f"Age: {age}")
+print(f"Address: {address}")
+
+
