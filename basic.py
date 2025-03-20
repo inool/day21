c5e7f61779086e9d9d9904f38fa432cbd87309d0
inool <inooljariya7@gmail.com>
2025-03-20 14:20:37 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,9 @@
+print("Hello, world!")
+name = "INOOLJARIYA"
+print("Hello, " + name + "!")
+num1 = 5
+num2 = 3
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
+age = 25
+print(f"{name} is {age} years old.")
